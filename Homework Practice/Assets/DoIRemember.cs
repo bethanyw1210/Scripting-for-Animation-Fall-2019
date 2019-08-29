cb5a3faabaede68312d697b75a27c40e161e6857
Bethany Woodruff <bethany.w1210@gmail.com>
2019-08-28 20:27:27 -0600

    Testing to see how much I remembered

---
+++ Homework Practice/Assets/DoIRemember.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoIRemember : MonoBehaviour
+{
+	public bool correctFunction;
+
+	// Use this for initialization
+	void Start ()
+	{
+		if (correctFunction == true)
+			print("I remembered some coding stuff!");
+		
+		else
+			print("I didn't remember :(");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
