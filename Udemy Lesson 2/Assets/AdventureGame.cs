da59d286db7f0a0f57659223d50c3187b2947990
Bethany Woodruff <bethany.w1210@gmail.com>
2019-09-06 21:40:09 -0600

    continued to work on Udemy course

--- Udemy Lesson 2/Assets/AdventureGame.cs
+++ Udemy Lesson 2/Assets/AdventureGame.cs
@@ -1,12 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class AdventureGame : MonoBehaviour {
+public class AdventureGame : MonoBehaviour
+{
+	[SerializeField] Text textComponent;
+	[SerializeField] State startingState;
+
+	State state;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+		state = startingState;
+		textComponent.text = state.GetStateStory();
+
 	}
 	
 	// Update is called once per frame
---
+++ Udemy Lesson 2/Assets/State.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    [TextArea(20,16)] [SerializeField] string storyText;
+
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+}
