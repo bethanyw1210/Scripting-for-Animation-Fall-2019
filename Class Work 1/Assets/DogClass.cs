659da6fe7005ed4cdd90a901bb5d9deae02dadc7
Bethany Woodruff <bethany.w1210@gmail.com>
2019-09-12 15:49:07 -0600

    Worked on particle even systems

---
+++ Class Work 1/Assets/DogClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DogClass : MonoBehaviour
+{
+    public UnityEvent doesStuff;
+    
+    private void OnMouseDown()
+    {
+        //Dog will do stuff when the mouse is button is pressed
+        //Invoke calls a function
+        doesStuff.Invoke();
+    }
+}
---
+++ Class Work 1/Assets/ParticleControl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleControl : MonoBehaviour
+{
+    public ParticleSystem particles;
+    
+    // Start is called before the first frame update
+    private void OnMouseDown()
+    {
+        particles.Emit(count: 100);
+    }
+}
