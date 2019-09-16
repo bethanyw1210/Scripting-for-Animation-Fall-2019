e1c1d00054689db7d2b23cda5664d24bbd2f680a
Bethany Woodruff <bethany.w1210@gmail.com>
2019-09-15 19:18:33 -0600

    added movement script to character

--- Class Work 1/Assets/CatClass.cs
+++ Class Work 1/Assets/CatClass.cs
@@ -11,15 +11,27 @@ public class CatClass : MonoBehaviour
 
     public UnityEvent orangeParticles;
     public UnityEvent blueParticles;
+    private float moveSpeed = 5;
 
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.A))
         {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed,GetComponent<Rigidbody2D>().velocity.y);
+            gameObject.transform.localScale = new Vector3(1,1,1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed,GetComponent<Rigidbody2D>().velocity.y);
+            gameObject.transform.localScale = new Vector3(-1, 1, 1);
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
             OrangeParticles();
         }
-        else if (Input.GetKeyDown(KeyCode.D))
+        else if (Input.GetKeyDown(KeyCode.LeftShift))
         {
             BlueParticles();
         }
