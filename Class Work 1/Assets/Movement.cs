21424ec2ed94d49d18e054cf9788330370e1ccd8
Bethany Woodruff <bethany.w1210@gmail.com>
2019-09-17 15:55:24 -0600

    added movement script to character

---
+++ Class Work 1/Assets/Movement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Vector3 position;
+    public CharacterController controller;
+    private float speed = 5f;
+    private float gravity = -9.81f;
+    private float jumpSpeed = 100f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.Translate(position*Time.deltaTime);
+        //! means is not
+        position.x = speed * Input.GetAxis("Horizontal");
+
+        if (!controller.isGrounded)
+        {
+            position.y += gravity;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            position.y = jumpSpeed;
+        }
+        
+        controller.Move(position * Time.deltaTime);
+
+
+    }
+}
