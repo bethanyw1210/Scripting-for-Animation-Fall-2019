d4b44afbf5d3786e1b030d630f092e25f31952c5
Bethany Woodruff <bethany.w1210@gmail.com>
2019-09-24 15:51:49 -0600

    worked on adding forces and character movements/jump

---
+++ Class Work 1/Assets/IfStatements.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfStatements : MonoBehaviour
+{
+    public int a = 4;
+    public int b = 5;
+    public int c = 10;
+    public string password = "Friend";
+    public bool lightsOn = true;
+    void Start()
+    {
+        if (a + b == c)
+        {
+            print(true);
+        }
+
+        if (password == "0U812")
+        {
+            print(true);
+        }
+
+        if (lightsOn)
+        {
+            print(true);
+        }
+    }
+    
+}
--- Into the Forest/Assets/Scripts/CharMovement.cs
+++ Into the Forest/Assets/Scripts/CharMovement.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class CharMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private float moveSpeed = 5, gravity = -9.81f;
+    private int jumpHeight = 5;
+    private bool grounded = true;
+    private Rigidbody2D pcRigid;
+
+    // Use this for initialization 
     void Start()
     {
-        
+        grounded = true;
+        pcRigid = gameObject.GetComponent<Rigidbody2D>();
+
     }
 
-    // Update is called once per frame
+    // Update is called once per frame 
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.D))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+            gameObject.transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+            gameObject.transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+            
+            if (grounded == false)
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight * gravity);
+            }
+        }
     }
 }
