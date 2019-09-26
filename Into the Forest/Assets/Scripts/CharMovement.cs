ce86ae7e4e5062dfa08b4c5f30de03e8f44ebe27
Bethany Woodruff <bethany.w1210@gmail.com>
2019-09-25 23:15:15 -0600

    Trying to figure out jump on character

--- Into the Forest/Assets/Scripts/CharMovement.cs
+++ Into the Forest/Assets/Scripts/CharMovement.cs
@@ -1,14 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CharMovement : MonoBehaviour
 {
     private float moveSpeed = 5, gravity = -9.81f;
-    private int jumpHeight = 5;
+    private int jumpHeight = 10;
     private bool grounded = true;
     private Rigidbody2D pcRigid;
-
+    public Vector2 position;
     // Use this for initialization 
     void Start()
     {
@@ -31,15 +33,28 @@ public class CharMovement : MonoBehaviour
             GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
             gameObject.transform.localScale = new Vector3(-1, 1, 1);
         }
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+            position.y += gravity;
+        } 
+        
+    }
+    /*void OnCollisionEnter(Collision other)
+    {
+        if (GameObject.FindGameObjectWithTag("Ground"))
+        {
+            Jump();
+        }
+    }
 
-        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+    void Jump()
+    {
+        if (Input.GetKey(KeyCode.Space) && (GameObject.FindGameObjectWithTag("Ground")))
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
             
-            if (grounded == false)
-            {
-                GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight * gravity);
-            }
-        }
-    }
+        } 
+    }*/
 }
