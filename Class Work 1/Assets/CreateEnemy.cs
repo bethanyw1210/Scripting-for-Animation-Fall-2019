9c96aa4acea840254b7a11501879afdf2d2d0467
Bethany Woodruff <bethany.w1210@gmail.com>
2019-09-29 20:40:54 -0600

    Fixed Instantiate Player/Enemy

---
+++ Class Work 1/Assets/CreateEnemy.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateEnemy : MonoBehaviour
+{
+    public string enemyName;
+    private int myNumber;
+    public float speed = 5f;
+    public GameObject enemy;
+    public Color enemyColor = Color.red;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ConfigureGameObject();
+    }
+
+    private GameObject ConfigureGameObject()
+    {
+        var newPlayer = Instantiate(enemy);
+        newPlayer.layer = 0;
+        newPlayer.active = true;
+        newPlayer.tag = "Player";
+        newPlayer.transform.position = Vector3.zero;
+        newPlayer.name = enemyName;
+        newPlayer.GetComponent<Renderer>().material.color = enemyColor;
+
+        return newPlayer;
+    }
+
+    private string WelcomePlayer()
+    {
+        return "Oh no! " + enemyName + " has appeared!";
+    }
+
+    private int AddNumbers(int a, int b)
+    {
+        print(a + b);
+        return a + b;
+    }
+
+    private float IncreaseSpeed(float multiplier)
+    {
+        return speed * multiplier;
+    }
+    
+}
--- Class Work 1/Assets/Functions.cs
+++ Class Work 1/Assets/Functions.cs
@@ -1,78 +1,49 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Functions : MonoBehaviour
-{
-    public string playerName;
-    private int myNumber;
-    public float speed = 5f;
-    public GameObject player, wizard, soldier;
-    public Color playerColor = Color.green;
-    public Color wizardColor = Color.blue;
-    public Color soldierColor = Color.red;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        ConfigureGameObject();
-        ConfigureGameObjectWizard();
-        ConfigureGameObjectSoldier();
-    }
-
-    private GameObject ConfigureGameObject()
-    {
-        var newPlayer = Instantiate(player);
-        newPlayer.layer = 0;
-        newPlayer.active = true;
-        newPlayer.tag = "Player";
-        newPlayer.transform.position = Vector3.zero;
-        newPlayer.name = playerName;
-        newPlayer.GetComponent<Renderer>().material.color = playerColor;
-
-        return newPlayer;
-    }
-
-    private GameObject ConfigureGameObjectWizard()
-    {
-        var newPlayer = Instantiate(wizard);
-        newPlayer.layer = 0;
-        newPlayer.active = true;
-        newPlayer.tag = "Wizard";
-        newPlayer.transform.position = Vector3.zero;
-        newPlayer.name = playerName;
-        newPlayer.GetComponent<Renderer>().material.color = wizardColor;
-
+﻿using System.Collections; 
+using System.Collections.Generic; 
+using UnityEngine; 
+ 
+public class Functions : MonoBehaviour 
+{ 
+    public string playerName; 
+    private int myNumber; 
+    public float speed = 5f; 
+    public GameObject player; 
+    public Color playerColor = Color.green; 
+     
+    // Start is called before the first frame update 
+    void Start() 
+    { 
+        ConfigureGameObject(); 
+    } 
+   
+    private GameObject ConfigureGameObject() 
+    { 
+        var newPlayer = Instantiate(player); 
+        newPlayer.layer = 0; 
+        newPlayer.active = true; 
+        newPlayer.tag = "Player"; 
+        newPlayer.transform.position = Vector3.zero; 
+        newPlayer.name = playerName; 
+        newPlayer.GetComponent<Renderer>().material.color = playerColor; 
+ 
         return newPlayer; 
-    }
-    
-    private GameObject ConfigureGameObjectSoldier()
-    {
-        var newPlayer = Instantiate(soldier);
-        newPlayer.layer = 0;
-        newPlayer.active = true;
-        newPlayer.tag = "Wizard";
-        newPlayer.transform.position = Vector3.zero;
-        newPlayer.name = playerName;
-        newPlayer.GetComponent<Renderer>().material.color = soldierColor;
-
-        return newPlayer; 
-    }
-
-    private string WelcomePlayer()
-    {
-        return "Welcome to the game, " + playerName + "!";
-    }
-
-    private int AddNumbers(int a, int b)
-    {
-        print(a + b);
-        return a + b;
-    }
-
-    private float IncreaseSpeed(float multiplier)
-    {
-        return speed * multiplier;
-    }
-    
+    } 
+     
+     
+    private string WelcomePlayer() 
+    { 
+        return playerName + "Welcome to the game!"; 
+    } 
+    private int AddNumbers(int a, int b) 
+    { 
+        print(a + b); 
+        return a + b; 
+    } 
+ 
+    private float IncreaseSpeed(float multiplier) 
+    { 
+        return speed * multiplier; 
+    } 
+ 
+   
 }
