f35f39faf38fffbf83393becf316af6518f5e7c2
Bethany Woodruff <bethany.w1210@gmail.com>
2019-10-01 15:44:59 -0600

    Created score bar

---
+++ Class Work 1/Assets/Initiate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu] //essential line for scriptable object 
+public class Initiate : ScriptableObject // essential for scriptable object
+{
+    public int value = 1;
+
+    public void UpdateValue(int number)
+    {
+        value += number;
+    }
+}
---
+++ Class Work 1/Assets/UITextControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+
+public class UITextControl : MonoBehaviour
+{
+    public UnityEvent startEvent;
+    private Text textLabel;
+
+    public void UpdateText(Initiate data )
+    {
+        textLabel.text = data.value.ToString();
+    }
+    
+    void Start()
+    {
+        textLabel = GetComponent<Text>();
+        startEvent.Invoke();
+    }
+}
