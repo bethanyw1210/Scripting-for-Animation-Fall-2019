a3667c8aebc4019cd3e42c12ba4797695f8bb04b
Bethany Woodruff <bethany.w1210@gmail.com>
2019-10-24 15:50:14 -0600

    added weapons list to game

---
+++ Class Work 1/Assets/GameArtCollections.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+
+public class GameArtCollections : ScriptableObject
+{
+    public List<GameArtData> collectionList;
+
+    public void AddData(GameArtData obj)
+    {
+        for (int i = 0; 1 < collectionList.Count; i++)
+        {
+            if (!collectionList.Contains(obj))
+            {
+                collectionList.Add(obj);
+            }
+        }
+    }
+    
+    public void RemoveLastItem(GameArtData obj)
+    {
+        collectionList.RemoveAt(collectionList.Count - 1);
+    }
+}
--- Class Work 1/Assets/GameArtData.cs
+++ Class Work 1/Assets/GameArtData.cs
@@ -7,4 +7,5 @@ public class GameArtData : ScriptableObject
     public Sprite sprite;
     public Color color;
     public GameObject prefab;
+    private bool collected;
 }
