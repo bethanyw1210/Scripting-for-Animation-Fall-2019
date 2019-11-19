d23aad6e72c39e5b0cc0bceb93a738924d079711
Bethany Woodruff <bethany.w1210@gmail.com>
2019-11-19 15:53:04 -0700

    added animator to game

---
+++ Class Work 1/Assets/Animation/ButtonInput.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ButtonInput : MonoBehaviour
+{
+    public UnityEvent inputEvent;
+    public string inputWord;
+    void Update()
+    {
+        if (Input.GetButtonDown(inputWord))
+        {
+            inputEvent.Invoke();
+        }
+    }
+}
