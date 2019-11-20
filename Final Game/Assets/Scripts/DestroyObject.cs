8a37ee2ead44ff37ac18d4160cd81cbc5b3d848a
Bethany Woodruff <bethany.w1210@gmail.com>
2019-11-20 16:50:08 -0700

    started on powerups

---
+++ Final Game/Assets/Scripts/DestroyObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObject : MonoBehaviour
+{
+    public float destroyTime = 8f;
+    void Start()
+    {
+        Destroy(gameObject, destroyTime);
+    }
+
+}
