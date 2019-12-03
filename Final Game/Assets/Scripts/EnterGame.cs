00836c290cf9581312c8c15f82980c4bfb93e4d2
Bethany Woodruff <bethany.w1210@gmail.com>
2019-12-02 21:01:55 -0700

    added buttons/script to pages and made first build

---
+++ Final Game/Assets/Scripts/EnterGame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnterGame : MonoBehaviour
+{
+    public string sceneName;
+    
+    public void Game()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
