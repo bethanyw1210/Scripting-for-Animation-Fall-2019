4d72c10174b8b8a2f7903c1c1fc378ad84cbf74b
Bethany Woodruff <bethany.w1210@gmail.com>
2019-12-05 11:59:05 -0700

    started on pause menu

---
+++ Final Game/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public bool paused = true;
+    float timesaver;
+
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(paused)
+            {
+                timesaver = Time.timeScale;
+                Time.timeScale = 0;
+                paused = false;
+            }
+            else
+            {
+                Time.timeScale = timesaver;
+                Pause();
+                paused = true;
+            }
+        }
+    }
+    
+    public void Pause()
+    {
+        SceneManager.LoadScene("Pause");
+    }
+}
